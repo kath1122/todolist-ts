c8cfa7c540816f1a11c149ea86531cf24ce5567b
katherine <katherine.tseng@skylinetw.com>
2024-03-04 18:28:00 +0800

    編輯保存

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,13 +3,15 @@
     <ul v-for="(item, index) in items" :key="item.id">
       <li>
         {{item.text}}
+        <button type="button" @click="editItem(item, index)">編輯</button>
         <button type="button" @click="deleteItem(index)">刪除</button>
       </li>
     </ul>
   </div>
   <hr>
   <div class="todo-footer">
-    {{ cacheData }}
+    <input type="text" v-model="cacheData.text">
+    <button type="button" @click="saveItem">保存</button>
   </div>
 </template>
 
@@ -20,24 +22,36 @@
 import { defineComponent, ref } from 'vue'
 import { useTodoStore } from '../stores/todo'
 
+import type { Todo, CacheData } from '@/types/todo'
+
 export default defineComponent({
   name: 'TodoList',
   setup: () => {
+    let cacheData = ref<CacheData>({id: 0, text: '', index: 0})
     const todoStore = useTodoStore()
-    let cacheData = {}
     const items = todoStore.todoItems
     
     const deleteItem = (index: number) => {
       todoStore.deleteItem(index)
     }
-    // const saveItem = () => {
+    const editItem = (item: Todo, index: number) => {
+      cacheData.value = {...item, index}
+    }
 
-    // }
+    const saveItem = () => {
+      if(!cacheData.value.text){
+        return
+      }
+      todoStore.updateItem(cacheData.value.text, cacheData.value.index)
+      cacheData.value.text = ''
+    }
     
     return {
       items,
       cacheData,
       deleteItem,
+      editItem,
+      saveItem
     }
   }
 })
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -2,3 +2,10 @@ export interface Todo {
     id: number,
     text: string
 }
+
+export interface CacheData {
+    id: number;
+    text: string;
+    index: number
+}
+  
