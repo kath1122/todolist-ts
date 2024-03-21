19a98643b0b307ac28f988070c3578c4e385ab71
katherine <katherine.tseng@skylinetw.com>
2024-03-21 17:51:24 +0800

    add searchItem

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,7 @@
 <template>
     <div>
-      <!-- TODO 相同的css程式碼抽出來 -->
-      <input class="w-52 border border-gray-300 px-4 py-2 rounded-l focus:border-green-500 focus:outline-none" type="text" v-model="searchInput" placeholder="Search a todo item.">
-      <button class="bg-green-600 text-white mx-1 my-1.5 px-4 py-2 rounded cursor-pointer" type="button" @click="searchItem">Search</button>
-    </div>
-    <div>
-      <input class="w-52 border border-gray-300 px-4 py-2 rounded-l focus:border-green-500 focus:outline-none" type="text" v-model="newItem" placeholder="Enter a todo item.">
-      <button class="bg-green-600 text-white mx-1 my-1.5 px-4 py-2 rounded cursor-pointer" type="button" @click="addItem">Submit</button>
+      <input class="padding w-52 border border-gray-300 rounded-l focus:border-green-500 focus:outline-none" type="text" v-model="newItemInputText" placeholder="Enter a todo item.">
+      <button class="padding bg-green-600 text-white mx-1 my-1.5 rounded cursor-pointer" type="button" @click="addItem">Submit</button>
     </div>
   </template>
 
@@ -16,27 +11,25 @@
   import type { Todo } from '@/types/todo';
 
   const todoStore = useTodoStore()
-  // TODO newItem的意義是一個輸入值 newItemInputText or InputText or newItemText will be better
-  const newItem = ref<string>('')
-  // TODO searchInput的意義是一個輸入值 searchInputText or  searchText or queryText will be better
-  const searchInput = ref<string>('')
+  const newItemInputText = ref<string>('')
 
   const addItem = () => {
-    if(!newItem.value.trim()) {
+    if(!newItemInputText.value.trim()) {
       console.log('請輸入值')
       return
     }
     const todo: Todo = {
       id: Date.now(),
-      text: newItem.value,
+      text: newItemInputText.value,
       isComplete: false
     }
     todoStore.addItem(todo)
-    newItem.value = ''
+    newItemInputText.value = ''
   }
-  const searchItem = () => {
-    // const term = searchInput.value.toLowerCase();
-    // todoStore.todoItems = todoStore.todoItems.filter(task => task.text.toLowerCase().includes(term));
-  }
-
   </script>
+
+<style lang="postcss" scoped>
+.padding {
+  @apply px-4 py-2;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,15 @@
 <template>
-  <div v-if="todoItems && todoItems.length">
+  <div>
+      <input class="padding w-52 border border-gray-300 rounded-l focus:border-green-500 focus:outline-none" type="text" v-model="searchInputText" placeholder="Search a todo item.">
+      <button class="padding bg-green-600 text-white mx-1 my-1.5 rounded cursor-pointer" type="button" @click="searchItem">Search</button>
+  </div>
+  <div v-if="!isSearch && todoItems && todoItems.length">
     <ul v-for="(item, index) in todoItems" :key="`${item.id}${index}`">
+      <TodoItem :item="item" :index="index"></TodoItem>
+    </ul>
+  </div>
+  <div v-else>
+    <ul v-for="(item, index) in todoArr" :key="`${item.id}${index}`">
       <TodoItem :item="item" :index="index"></TodoItem>
     </ul>
   </div>
@@ -10,8 +19,34 @@
 import { useTodoStore } from '../stores/todo'
 import { storeToRefs } from 'pinia';
 import TodoItem from './TodoItem.vue'
+import { computed, ref, watch } from 'vue';
 
+const searchInputText = ref<string>('')
 const todoStore = useTodoStore()
 const { todoItems } = storeToRefs(todoStore)
-    
+const isSearch = ref(false)
+let todoArr = ref([])
+
+const searchItem = () => {
+  const term = searchInputText.value.toLowerCase();
+  if(term){
+    isSearch.value = true
+  } else {
+    isSearch.value = false
+  }
+  todoArr.value = todoStore.todoItems.filter(task => task.text.toLowerCase().includes(term));
+}
+
+watch(searchInputText, (newValue) => {
+  if(!newValue){
+    isSearch.value = false
+  }
+});
+
 </script>
+
+<style lang="postcss" scoped>
+.padding {
+  @apply px-4 py-2;
+}
+</style>
