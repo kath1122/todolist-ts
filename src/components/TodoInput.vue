8ab0052c8e7075087a5c031ddce5e43988c6dd17
katherine <katherine.tseng@skylinetw.com>
2024-03-28 17:37:30 +0800

    add todo expiration time UI

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <input class="px-4 py-2 w-52 border border-gray-300 rounded-l focus:border-green-500 focus:outline-none" type="text" v-model="newItemInputText" placeholder="Enter a todo item.">
-    <button class="px-4 py-2 bg-green-600 text-white mx-1 my-1.5 rounded cursor-pointer" type="button" @click="addItem">Submit</button>
+    <el-input class="max-w-[600px] w-52 rounded-l focus:border-green-500 focus:outline-none"
+      type="text" 
+      v-model="newItemInputText.text"
+      placeholder="Enter a todo item.">
+    </el-input>
+    <el-date-picker
+      v-model="newItemInputText.date"
+      type="date"
+      placeholder="Select date">
+    </el-date-picker>
+    <el-button class="px-4 py-2 bg-green-600 text-white mx-1 my-1.5 rounded cursor-pointer" @click="addItem">Submit</el-button>
   </div>
 </template>
 
@@ -11,20 +20,26 @@
   import type { Todo } from '@/types/todo';
 
   const todoStore = useTodoStore()
-  const newItemInputText = ref<string>('')
+  const newItemInputText = ref<{ text: string; date: Date | null }>({
+    text: '',
+    date: null
+  })
+
 
   const addItem = () => {
-    if(!newItemInputText.value.trim()) {
+    if(!newItemInputText.value.text.trim()) {
       console.log('請輸入值')
       return
     }
     const todo: Todo = {
       id: Date.now(),
-      text: newItemInputText.value,
+      text: newItemInputText.value.text,
+      date: newItemInputText.value.date,
       isComplete: false,
       isShow: true
     }
     todoStore.addItem(todo)
-    newItemInputText.value = ''
+    newItemInputText.value.text = ''
+    newItemInputText.value.date = null
   }
 </script>
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -2,5 +2,6 @@ export interface Todo {
     id?: number,
     text: string,
     isComplete: boolean,
-    isShow?: boolean
+    isShow?: boolean,
+    date?: Date | null;
 }
